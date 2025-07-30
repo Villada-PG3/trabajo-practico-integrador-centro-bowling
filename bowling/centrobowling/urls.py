98972b6486793d72ee838026df094d866dee1561
AdriR1ze <adriano.mancuso.2008@gmail.com>
2025-07-30 12:16:59 -0300

    boceto

--- bowling/centrobowling/urls.py
+++ bowling/centrobowling/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bowl.views import inicio, reserva
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', inicio, name="inicio"),
+    path('reserva/', reserva, name="reserva")
 ]
