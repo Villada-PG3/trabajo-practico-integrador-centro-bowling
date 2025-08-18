4be8ee35fc457797ecce96487b27cf935bb347e0
AdriR1ze <adriano.mancuso.2008@gmail.com>
2025-08-18 13:07:51 +0000

    reserva update

--- bowling/bowl/views.py
+++ bowling/bowl/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Reserva
 
 # Create your views here.
 from django.shortcuts import render
@@ -7,3 +9,9 @@ def inicio(request):
     return render(request, 'bowl/inicio.html')
 def reserva(request):
     return render(request, 'bowl/reserva.html')
+
+
+class ReservaView(ListView):
+    model = Reserva
+    template_name = "bowl/reservas.html"  # tu HTML
+    context_object_name = "reservas"  # nombre que usás en el template
