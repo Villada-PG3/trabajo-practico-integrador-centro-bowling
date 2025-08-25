32f11bfface4c56265f3b8227e3cf4d01c8c0e74
MarianoNicolasFerreyra <nicoferreyra2612@gmail.com>
2025-08-25 10:12:32 -0300

    modifico el ER

--- bowling/bowl/views.py
+++ bowling/bowl/views.py
@@ -5,19 +5,11 @@ from .models import Reserva
 
 def inicio(request):
     return render(request, 'bowl/inicio.html')
+
 def reserva(request):
     return render(request, 'bowl/reserva.html')
 
-
-
-
-#class ReservaView(LoginRequiredMixin, ListView):
 class ReservaView(ListView):
     model = Reserva
-    template_name = 'bowl/reserva.html'  # tu template
-    context_object_name = 'reservas'
-
-    def get_queryset(self):
-        # Filtra solo las reservas del cliente logueado
-        return Reserva.objects.all
-        #return Reserva.objects.filter(cliente__user=self.request.user)
+    template_name = "bowl/reservas.html"
+    context_object_name = "reservas"
