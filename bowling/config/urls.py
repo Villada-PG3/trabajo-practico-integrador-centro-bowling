c5868f5ac700f16bcfcebd5b87c1713b30dedc24
AdriR1ze <adriano.mancuso.2008@gmail.com>
2025-09-15 15:03:03 +0000

    lista-crear-editar

--- bowling/config/urls.py
+++ bowling/config/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from bowl.views import (
     InicioView, CafeView, IniciarSesionView,
-    ReservaView, ListaPistasView, CrearPistaView, EditarPistaView
+    ReservaView, ListaPistasView, CrearPistaView, EditarPistaView, ListaComidaView, CrearCafeteriaView, EditarCafeteriaView,
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -20,4 +20,9 @@ urlpatterns = [
     path('pistas/', ListaPistasView.as_view(), name="lista_pistas"),
     path('pistas/crear/', CrearPistaView.as_view(), name="crear_pista"),
     path('pistas/<int:pk>/editar/', EditarPistaView.as_view(), name="editar_pista"),
+    
+    #Cafeteria
+    path('cafeteria/', ListaComidaView.as_view(), name="lista_comida"),
+    path('cafeteria/crear/', CrearCafeteriaView.as_view(), name="crear_comida"),
+    path('cafeteria/<int:pk>/editar/', EditarCafeteriaView.as_view(), name="editar_comida"),
 ]
